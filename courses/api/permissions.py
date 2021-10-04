5a282713dd9a09365034b8b47a80b32f0ed6243c
alex <martsenyk.l@gmail.com>
2021-10-04 14:37:00 +0300

    Create your own permissions

---
+++ courses/api/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsEnrolled(BasePermission):
+    """
+    Проверяем, является ли текущий пользователь слушателем курса, 
+    через атрибут students объекта модели Course.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.students.filter(id=request.user.id).exists()
